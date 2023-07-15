b33da80c7c57fa9e68d6f8ac3a7e710f714b027d
gaurav-singh-mrg <gauravsinghmr.g@gmail.com>
2023-07-16 00:07:02 +0530

    54) Css Implementation for profile settings. Needs more work

--- static/jscode/main.js
+++ static/jscode/main.js
@@ -3,5 +3,4 @@ function hideElement(elm){
     const a = document.getElementsByClassName(elm);
     a[0].removeAttribute("class","msg")
     a[0].setAttribute("class","hidemsg")
-    //a[0].style.display= 'none';
 }
--- static/jscode/profile.js
+++ static/jscode/profile.js
@@ -1,44 +1,39 @@
 
 function accordionShowHide(id , set){
-    const a = document.getElementsById(id)
+    const a = document.getElementById(id)
     console.log(a)
     console.log('Gauravsingh')
 //    a.removeAttribute("class")
 }
-function onClickMedia(elm , set){
-    const a = document.getElementsByClassName(elm);
-    a[0].removeAttribute("class",elm)
-    a[0].setAttribute("class",set)
+function onClickMedia(elm , set) {
+    let a;
+    a = document.getElementsByClassName(elm);
+    a[0].removeAttribute("class", elm)
+    a[0].setAttribute("class", set)
 }
 
 function btn(id){
     const a = document.getElementById(id);
     const b = a.classList
-    if(b[0] == 'show')
-    {
-        a.removeAttribute("class","show")
+    if (b[0] === 'show') {
+        a.removeAttribute('class', "show")
         a.setAttribute("class", "collapse")
-    }
-    else
-         a.setAttribute("class", "show")
+    } else
+        a.setAttribute("class", "show")
 }
 
 
 function ChangePicture(src,target){
     const a = document.getElementById(src)
     console.log(a)
-
     console.log(a.value)
-    {% comment %} alert('Must Select any of your photo for upload!'); {% endcomment %}
 }
 
 
 function showImage(src,target) {
   var fr=new FileReader();
-  // when image is loaded, set the src of the image where you want to display it
   fr.onload = function(e) { target.src = this.result; };
   src.addEventListener("change",function() {
-    // fill fr with image data
     fr.readAsDataURL(src.files[0]);
   });
 }
