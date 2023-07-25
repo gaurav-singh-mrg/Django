a3d8b66aee0ffc363a1dab4cef1e2746d606b6ea
gaurav-singh-mrg <gauravsinghmr.g@gmail.com>
2023-07-25 08:27:34 +0530

    65) added login decorater in class based views

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, TemplateView
 
 
@@ -9,5 +10,8 @@ def homepage(request):
     return render(request, "Home/home.html")
 
 
+# we cand also use login decorator in url config file  like
+# login_required(TemplateView.as_view(template_name="secret.html"))
+@method_decorator(login_required, name="dispatch")
 class Homepage(TemplateView):
     template_name = 'Home/home.html'
